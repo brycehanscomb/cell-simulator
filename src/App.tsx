c89cb44e7485f8f9f3aaaa612d647575d8378ddf
Bryce Hanscomb <bryce@brycehanscomb.com>
2020-03-28 09:48:19 +1100

    ensure the URL doesn't get clobbered on each popstate by the url change watcher

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,53 @@ import './App.css';
 import Board from "./components/Board";
 import {ALIVE, BoardState, CellValue, DEAD, getNextGeneration} from "./util/game";
 
+export interface GameState {
+    boardState: BoardState;
+    rows: number;
+    cols: number;
+}
+
+const parseMap = {
+    '0': DEAD,
+    '1': ALIVE
+};
+
+const hashState = (boardState: BoardState, rows: number, cols: number) : string => {
+    return `${boardState.join('')}@${cols}x${rows}`
+}
+
+const readStateFromUrl = () : GameState => {
+    const { searchParams } = new URL(window.location.href)
+
+    let boardState: BoardState;
+    let rows: number;
+    let cols: number;
+
+    if (searchParams.has('cols')) {
+        cols = parseInt(searchParams.get('cols')!, 10)
+    } else {
+        cols = 8
+    }
+
+    if (searchParams.has('rows')) {
+        rows = parseInt(searchParams.get('rows')!, 10)
+    } else {
+        rows = 8
+    }
+
+    if (searchParams.has('boardState')) {
+        // todo: validate values in here and also check for board size params
+        boardState = searchParams.get('boardState')!.split('').map((value) => parseMap[value as '0'|'1'] as CellValue)
+    } else {
+        boardState = Array(rows * cols).fill(DEAD)
+    }
+
+    return {
+        boardState,
+        rows,
+        cols
+    }
+}
 
 let initialState: BoardState = [
     0, 0, 0, 0, 0, 0, 0, 0,
@@ -14,29 +61,9 @@ let initialState: BoardState = [
     0, 0, 0, 0, 0, 0, 0, 0
 ];
 
-const { searchParams } = new URL(window.location.href)
+initialState = readStateFromUrl().boardState
 
-const parseMap = {
-    '0': DEAD,
-    '1': ALIVE
-};
-
-if (searchParams.has('boardState')) {
-    // todo: validate values in here and also check for board size params
-    initialState = searchParams.get('boardState')!.split('').map((value) => parseMap[value as '0'|'1'] as CellValue)
-}
-
-
-// const initialState: BoardState = [
-//   0,0,0,0,0,
-//   0,0,0,0,0,
-//   0,0,0,0,0,
-//   0,0,0,0,0,
-//   0,0,0,0,0,
-// ]
-
-const noop = () => {
-}
+const noop = () => {}
 
 const saveState = (boardState: BoardState, rows: number, cols: number) => {
     const url = new URL(window.location.href)
@@ -47,19 +74,38 @@ const saveState = (boardState: BoardState, rows: number, cols: number) => {
     window.history.pushState({ boardState, rows, cols}, '', url.toString())
 }
 
-function App() {
-    const [boardState, setBoardState] = useState<BoardState>(initialState)
+const initialGameState = readStateFromUrl()
 
-    const rows = 7
-    const cols = 8
+function App() {
+    const [boardState, setBoardState] = useState<BoardState>(initialGameState.boardState)
+
+    /**
+     * @todo: just keep these in mutable state like boardstate is
+     */
+    const rows = initialGameState.rows
+    const cols = initialGameState.cols
 
     const step = () => setBoardState(
         getNextGeneration(boardState, rows, cols)
     )
 
     useEffect(() => {
-        saveState(boardState, rows, cols)
+        if (hashState(boardState, rows, cols) !== hashState(readStateFromUrl().boardState, rows, cols)) { // todo: read ros/cols from url too
+            saveState(boardState, rows, cols)
+        }
     }, [boardState, rows, cols])
+
+    useEffect(() => {
+        const handleHistoryChange = ({ state }: any) => {
+            if (state) {
+                setBoardState(state.boardState);
+            }
+        };
+
+        window.addEventListener('popstate', handleHistoryChange)
+
+        return () => window.removeEventListener('popstate', handleHistoryChange)
+    }, [setBoardState]);
 
     return (
         <div className="App">
