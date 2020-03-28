c171afd30aaa75c080804498a48c6f62ad595431
Bryce Hanscomb <bryce@brycehanscomb.com>
2020-03-28 16:35:36 +1100

    add a more interesting sample board

--- src/components/Board/styled.ts
+++ src/components/Board/styled.ts
@@ -1,8 +1,8 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Root = styled.div<{ rows: number, cols: number }>`
+export const Root = styled.div<{ rows: number; cols: number }>`
   display: grid;
   grid-template-rows: repeat(${props => props.rows}, 50px);
   grid-template-columns: repeat(${props => props.cols}, 50px);
   grid-gap: 3px;
-`
+`;
