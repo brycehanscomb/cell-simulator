3ef4a84dfce353ad70de8de497d018c0acc8e19e
Bryce Hanscomb <bryce@brycehanscomb.com>
2020-03-29 16:46:28 +1100

    reduce duplication of string casting

--- src/util/color.ts
+++ src/util/color.ts
@@ -8,9 +8,9 @@
  * @param percent - an int from -100 to 100
  */
 export function shadeColor(color: string, percent: number) {
-  let R = parseInt(color.substring(1, 3), 16);
-  let G = parseInt(color.substring(3, 5), 16);
-  let B = parseInt(color.substring(5, 7), 16);
+  let R: number | string = parseInt(color.substring(1, 3), 16);
+  let G: number | string = parseInt(color.substring(3, 5), 16);
+  let B: number | string = parseInt(color.substring(5, 7), 16);
 
   R = parseInt(String((R * (100 + percent)) / 100));
   G = parseInt(String((G * (100 + percent)) / 100));
@@ -20,10 +20,13 @@ export function shadeColor(color: string, percent: number) {
   G = G < 255 ? G : 255;
   B = B < 255 ? B : 255;
 
-  // todo: don't keep casting toString() so repetitively here
-  let RR = R.toString(16).length === 1 ? "0" + R.toString(16) : R.toString(16);
-  let GG = G.toString(16).length === 1 ? "0" + G.toString(16) : G.toString(16);
-  let BB = B.toString(16).length === 1 ? "0" + B.toString(16) : B.toString(16);
+  R = R.toString(16);
+  G = G.toString(16);
+  B = B.toString(16);
+
+  const RR = R.length === 1 ? "0" + R : R;
+  const GG = G.length === 1 ? "0" + G : G;
+  const BB = B.length === 1 ? "0" + B : B;
 
   return `#${RR}${GG}${BB}`;
 }
